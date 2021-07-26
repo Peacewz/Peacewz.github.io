4bb7f6952e1b0e42a72d18da982215940a50d3b0
Peacewz <87770325+Peacewz@users.noreply.github.com>
2021-07-26 23:01:05 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -1,10 +1,5 @@
 document.addEventListener("DOMContentLoaded", function(event) {
     let targetImage = document.querySelector("#smart-image");
- targetImage.addEventListener("click", function() {
-  
-  if (targetImage.classList.contains("small")) {
-      targetImage.classList.remove("small");
-  } else {
-      targetImage.classList.add("small");
-  } 
- });
+     targetImage.addEventListener("click", function() {
+     targetImage.classList.remove("small");
+});
