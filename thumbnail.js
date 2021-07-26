cf09068d2c45b55008063321930237820341f53e
Peacewz <87770325+Peacewz@users.noreply.github.com>
2021-07-26 20:40:28 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -1,13 +1,9 @@
 document.addEventListener("DOMContentLoaded", function(event) {
     let targetImage = document.querySelector("#smart-image");
 if (targetImage.classList.contains("small")) {
-  targetImage.addEventListener("click", function() {
     targetImage.classList.remove("small");
-});
 } else {
-  targetImage.addEventListener("click", function() {
     targetImage.classList.add("small");
-});
 }
 
 });
